079f69aa0451b5824a9c05671015a0852f586f54
octowild <139123949+octowild@users.noreply.github.com>
2023-10-18 14:35:09 +0530

    Update roach.gd

--- Design Studio Game/scripts/roach.gd
+++ Design Studio Game/scripts/roach.gd
@@ -6,8 +6,8 @@ const SPEED = 300.0
 
 func _physics_process(delta):
 
-	var directionx = Input.get_axis("ui_left", "ui_right")
-	var directiony = Input.get_axis("ui_up", "ui_down")
+	var directionx = Input.get_axis("a", "d")
+	var directiony = Input.get_axis("w", "s")
 	
 	if directiony:
 		velocity.y=directiony*SPEED
