3d6ce6cc8ed797098030815ff5c682cb676a9624
octowild <139123949+octowild@users.noreply.github.com>
2023-10-18 14:20:52 +0530

    sdsv

--- Design Studio Game/scripts/roach.gd
+++ Design Studio Game/scripts/roach.gd
@@ -2,21 +2,19 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-
 
 
 func _physics_process(delta):
-	# Add the gravity.
 
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var directionx = Input.get_axis("ui_left", "ui_right")
+	var directiony = Input.get_axis("ui_up", "ui_down")
+	
+	if directiony:
+		velocity.y=directiony*SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	if directionx:
+		velocity.x = directionx * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
