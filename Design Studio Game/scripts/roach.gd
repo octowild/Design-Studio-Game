a95c048da5eed3dde1051a64eef2195f1b5ea6b4
octowild <139123949+octowild@users.noreply.github.com>
2023-10-19 18:21:42 +0530

    oogabooga

--- Design Studio Game/scripts/roach.gd
+++ Design Studio Game/scripts/roach.gd
@@ -4,8 +4,12 @@ extends CharacterBody2D
 @export var _maxspeed:int = 350
 @export var _accel:int=50
 
+@export var _spritedir:Vector2=Vector2(0,-1)
+
+@onready var _anim=$AnimationTree
+
 func _ready():
-	get_node("AnimatedSprite2D")
+	_updateanim(_spritedir)
 
 func _physics_process(delta):
 
@@ -13,7 +17,7 @@ func _physics_process(delta):
 		Input.get_action_strength("right")-Input.get_action_strength("left"),
 		Input.get_action_strength("down")-Input.get_action_strength("up")
 		)
- 
+	_updateanim(direction)
 	
 	if direction:
 		velocity.y= move_toward(velocity.y,_maxspeed*direction.y,_accel)
@@ -25,3 +29,6 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, _accel)
 
 	move_and_slide()
+func _updateanim(dir:Vector2):
+	if(dir!=Vector2.ZERO):
+		_anim.set("parameters/idle/blend_position",dir)
