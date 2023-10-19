58328c7e41dfeea80a96aed1986b9b988ba710a1
octowild <139123949+octowild@users.noreply.github.com>
2023-10-19 15:57:23 +0530

    yfcvghhg

--- Design Studio Game/scripts/3droach.gd
+++ Design Studio Game/scripts/3droach.gd
@@ -1,19 +1,25 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-
-
+@export var _maxspeed:float = 350
+@export var _accel:float=50
 
 func _physics_process(delta):
 
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = Vector2(
+		Input.get_action_strength("right")-Input.get_action_strength("left"),
+		Input.get_action_strength("down")-Input.get_action_strength("up")
+		)
+ 
+	
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.y= move_toward(velocity.y,_maxspeed*direction.y,_accel)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, _accel)
+	if direction:
+		velocity.x=move_toward(velocity.x,_maxspeed*direction.x,_accel)
+
+	else:
+		velocity.x = move_toward(velocity.x, 0, _accel)
 
 	move_and_slide()
--- Design Studio Game/scripts/roach.gd
+++ Design Studio Game/scripts/roach.gd
@@ -1,25 +1,26 @@
 extends CharacterBody2D
 
 
-var _maxspeed = 350
-
-var _accel=50
+@export var _maxspeed:int = 350
+@export var _accel:int=50
 
 func _ready():
 	get_node("AnimatedSprite2D")
 
 func _physics_process(delta):
 
-	var direction = vector2( Input.get_action_strength("right")-Input.get_action_strength("left"),Input.get_action_strength("down")-Input.get_action_strength("up"))
+	var direction = Vector2(
+		Input.get_action_strength("right")-Input.get_action_strength("left"),
+		Input.get_action_strength("down")-Input.get_action_strength("up")
+		)
  
 	
 	if direction:
-		velocity.y=move_toward(velocity.y,_maxspeed*direction.y,_accel)
+		velocity.y= move_toward(velocity.y,_maxspeed*direction.y,_accel)
 	else:
 		velocity.y = move_toward(velocity.y, 0, _accel)
 	if direction:
 		velocity.x=move_toward(velocity.x,_maxspeed*direction.x,_accel)
-
 	else:
 		velocity.x = move_toward(velocity.x, 0, _accel)
 
