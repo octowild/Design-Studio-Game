13fd7dab4b3d62d88503c6ce45cd588340c40ceb
Sidak Singh Kalsi <sidaksinghkalsi@gmail.com>
2023-10-27 01:35:47 +0530

    huh

--- Design Studio Game/scenes/Camera2D.gd
+++ Design Studio Game/scenes/Camera2D.gd
@@ -8,12 +8,3 @@ func _physics_process(delta):
 		Input.get_action_strength("right")-Input.get_action_strength("left"),
 		Input.get_action_strength("down")-Input.get_action_strength("up")
 		)
-	if direction:
-		velocity.y= move_toward(velocity.y,_maxspeed*direction.y,_accel)
-	else:
-		velocity.y = move_toward(velocity.y, 0, _accel)
-	if direction:
-		velocity.x=move_toward(velocity.x,_maxspeed*direction.x,_accel)
-	else:
-		velocity.x = move_toward(velocity.x, 0, _accel)
-		
