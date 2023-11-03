cc381dda80832b91a857fc4e893d339a8024a54f
octowild <139123949+octowild@users.noreply.github.com>
2023-11-03 23:41:21 +0530

    Update Camera2D.gd

--- Design Studio Game/scripts/Camera2D.gd
+++ Design Studio Game/scripts/Camera2D.gd
@@ -1,10 +1,4 @@
 extends Camera2D
 
-@export var _maxspeed:int = 150
-@export var _accel:int=50
-
 func _physics_process(delta):
-	var direction = Vector2(
-		Input.get_action_strength("right")-Input.get_action_strength("left"),
-		Input.get_action_strength("down")-Input.get_action_strength("up")
-		)
+	pass
