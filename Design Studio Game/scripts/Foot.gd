ea4c6f32b1cb68ed73be84f35474fd8a7e33203c
octowild <139123949+octowild@users.noreply.github.com>
2023-11-03 12:02:43 +0530

    hvbn

--- Design Studio Game/scripts/Foot.gd
+++ Design Studio Game/scripts/Foot.gd
@@ -19,19 +19,19 @@ func _physics_process(delta):
 	if (!_stamp):
 		_mcpos=_mc.position
 		position=lerp(position,_mcpos,_movespeed/100)
-		if(foot.position.y<=250):
+		if(foot.position.y>=-500):
 			foot.position.y-=50
 		#foot.position=lerp(foot.position,Vector2(position.x,position.y-1000),_fallspeed/200)
 		shadow.scale=lerp(shadow.scale,Vector2(0.5,0.5),_shadowspeed/25)
 	else:
-		foot.position.y=lerp(foot.position.y,position.y,_fallspeed/100)
+		foot.position.y=lerp(foot.position.y,float(0),_fallspeed/50)
 		shadow.scale=lerp(shadow.scale,Vector2(1,1),_shadowspeed/100)
 	print(foot.position)
 
 
 func _on_trig_body_entered(body):
 	_stamp=true
-	foot.position.y=position.y-250
+	foot.position.y=position.y-1000
 	_timer.start()
 
 func _on_timer_timeout():
@@ -39,6 +39,6 @@ func _on_timer_timeout():
 	_stamp=false
 
 func _on_kill_body_entered(body):
-	pass
-	#if(_contact):
-	#	_mcs._isded=true
+	
+	if(body.name=="WhiteMCRoach"&&_contact):
+		_mcs._isded=true
