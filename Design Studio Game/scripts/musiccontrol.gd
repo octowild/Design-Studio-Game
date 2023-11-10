f7fbb349b7f42a6bf672906a4c59c2fdfe0d883d
DevanarayanRajeev <88960864+DevanarayanRajeev@users.noreply.github.com>
2023-11-10 17:09:37 +0530

    sdfh

--- Design Studio Game/scripts/musiccontrol.gd
+++ Design Studio Game/scripts/musiccontrol.gd
@@ -8,9 +8,9 @@ func _ready():
 func _process(delta):
 	_curscn=get_tree().current_scene.name
 	if _curscn=="bathfloor1":
-		if Backgroundmusic.stream != load("res://objects/Goliath.mp3"):
+		if Backgroundmusic.stream != load("res://objects/Goliath_trimmed.mp3"):
 			Backgroundmusic.stop()
-			Backgroundmusic.stream = load("res://objects/Goliath.mp3")
+			Backgroundmusic.stream = load("res://objects/Goliath_trimmed.mp3")
 			Backgroundmusic.play()
 	elif _curscn=="pipes1":
 		if Backgroundmusic.stream != load("res://objects/No_Return_trimmed.mp3"):
