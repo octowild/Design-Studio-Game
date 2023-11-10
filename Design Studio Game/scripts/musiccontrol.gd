e2466426ddd5399b211956391de5afe6f240f6d9
DevanarayanRajeev <88960864+DevanarayanRajeev@users.noreply.github.com>
2023-11-10 15:47:49 +0530

    xrnyj

--- Design Studio Game/scripts/musiccontrol.gd
+++ Design Studio Game/scripts/musiccontrol.gd
@@ -9,6 +9,6 @@ func _ready():
 func _process(delta):
 	_curscn=get_tree().current_scene.name
 	if _curscn=="bathfloor1":
-		_musicplayer.source()
+		_musicplayer.source("res://objects/Goliath.mp3")
 	elif _curscn=="pipes1":
-		_musicplayer.source()
+		_musicplayer.source("res://objects/No_Return_trimmed.mp3")
