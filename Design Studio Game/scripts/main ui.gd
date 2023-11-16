c0b8bd759b7569de547bb4b0d022a8553d76dec9
octowild <139123949+octowild@users.noreply.github.com>
2023-11-16 15:26:49 +0530

    fdsdfs

--- Design Studio Game/scripts/main ui.gd
+++ Design Studio Game/scripts/main ui.gd
@@ -2,13 +2,27 @@ extends Control
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var label = $Label
+@onready var _timer = $Timer
+var fade=false
+var wasd=true
+var direction
 
 func _process(delta):
-	var direction = Vector2(
+		direction = Vector2(
 		Input.get_action_strength("right")-Input.get_action_strength("left"),
 		Input.get_action_strength("down")-Input.get_action_strength("up")
 		)
-	if direction:
-		animated_sprite_2d.modulate.a=lerp(animated_sprite_2d.modulate.a,0.0,0.1)
-		label.modulate.a=lerp(label.modulate.a,0.0,0.1)
-		
+		if direction&&wasd:
+			fade=true
+			_timer.start()
+		if fade:
+			animated_sprite_2d.modulate.a=lerp(animated_sprite_2d.modulate.a,0.0,0.05)
+			label.modulate.a=lerp(label.modulate.a,0.0,0.05)
+			wasd=false
+
+func _on_timer_timeout():
+	animated_sprite_2d.play("shift")
+	label.text=("SHIFT to Dash")
+	animated_sprite_2d.modulate.a=lerp(animated_sprite_2d.modulate.a,255.0,0.01)
+	label.modulate.a=lerp(label.modulate.a,255.0,0.01)
+	fade=false
