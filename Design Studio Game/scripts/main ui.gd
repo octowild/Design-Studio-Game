fc96ccd554d430d85907ccd2082e979874fa0300
octowild <139123949+octowild@users.noreply.github.com>
2023-11-16 14:59:56 +0530

    jgv

--- Design Studio Game/scripts/main ui.gd
+++ Design Studio Game/scripts/main ui.gd
@@ -8,4 +8,7 @@ func _process(delta):
 		Input.get_action_strength("right")-Input.get_action_strength("left"),
 		Input.get_action_strength("down")-Input.get_action_strength("up")
 		)
+	if direction:
+		animated_sprite_2d.modulate.a=lerp(animated_sprite_2d.modulate.a,0.0,0.1)
+		label.modulate.a=lerp(label.modulate.a,0.0,0.1)
 		
