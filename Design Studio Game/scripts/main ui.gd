90c3649c690a2d308102a75aca99cd4b3de90458
octowild <139123949+octowild@users.noreply.github.com>
2023-11-16 22:53:43 +0530

    sf

--- Design Studio Game/scripts/main ui.gd
+++ Design Studio Game/scripts/main ui.gd
@@ -3,14 +3,21 @@ extends Control
 @onready var animated_sprite_2d = $Panel/AnimatedSprite2D
 @onready var label = $Panel/Label
 @onready var _timer = $Timer
-@onready var _bg = $Panel
+@onready var _bg = $Panel/Panel
+@onready var retry = $Panel/retry
+@onready var ded = $"Panel/u died"
 
 @export var _mc:CharacterBody2D
 
 var fade=false
 var wasd=true
 var direction
-var new_style = StyleBoxFlat.new()
+
+func _ready():
+	_bg.modulate=Color(0,0,0,0)
+	retry.disabled=true
+	retry.modulate=Color(1,1,1,0)
+	ded.modulate=Color(1,1,1,0)
 
 func _process(delta):
 	direction = Vector2(
@@ -30,15 +37,20 @@ func _process(delta):
 		animated_sprite_2d.modulate.a=lerp(animated_sprite_2d.modulate.a,255.0,0.005)
 		label.modulate.a=lerp(label.modulate.a,255.0,0.005)
 	
-	#new_style.set_bg_color(Color(1,0,0,0))
-	if _mc._isded==true:
-		new_style.set_bg_color(Color(255,0,0,255))
-	
 
+	if _mc._isded==true:
+		_bg.modulate=lerp(_bg.modulate,Color(1,0,0,0.9),0.05)
+		retry.disabled=false
+		retry.modulate=Color(1,1,1,1)
+		ded.modulate=Color(1,1,1,1)
+		fade=true
 	
-	_bg.set('custom_styles/panel', new_style)
 
 func _on_timer_timeout():
 	animated_sprite_2d.play("shift")
 	label.text=("SHIFT to Dash")
 	fade=false
+
+
+func _on_retry_pressed():
+	get_tree().reload_current_scene()
