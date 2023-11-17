fc4cfe2283e9ae472ccc71d3072d9b25778f932c
octowild <139123949+octowild@users.noreply.github.com>
2023-11-17 13:13:26 +0530

    dsa

--- Design Studio Game/scripts/mainmenu.gd
+++ Design Studio Game/scripts/mainmenu.gd
@@ -1,19 +1,34 @@
 extends Control
 
+@onready var _cutscene = $VideoStreamPlayer
+@onready var play = $play
+@onready var quit = $quit
+@onready var credits = $credits
+@onready var skip = $skip
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	skip.visible=false
+	skip.modulate.a=0
+	
 func _process(delta):
 	pass
 
-
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/bathfloor1.tscn")
+	play.visible=false
+	quit.visible=false
+	credits.visible=false
+	skip.visible=true
+	skip.modulate.a=0.8
+	_cutscene.play()
+	
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+func _on_video_stream_player_finished():
+	get_tree().change_scene_to_file("res://scenes/bathfloor1.tscn")
+
+func _on_skip_pressed():
+	get_tree().change_scene_to_file("res://scenes/bathfloor1.tscn")
+	print("skip")
