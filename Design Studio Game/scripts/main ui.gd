5334ba720400c49b56ae030dbd645754e75c95a8
octowild <139123949+octowild@users.noreply.github.com>
2023-11-27 11:31:05 +0530

    game almosst done

--- Design Studio Game/scripts/main ui.gd
+++ Design Studio Game/scripts/main ui.gd
@@ -8,6 +8,7 @@ extends Control
 @onready var _return = $Panel/return
 @onready var deathscreen = $Panel/deathscreen
 @onready var endscreen = $Panel/endscreen
+@onready var pipesarc = $Panel/pipesarc
 
 @export var _mc:CharacterBody2D
 
@@ -23,6 +24,7 @@ func _ready():
 	_return.modulate=Color(1,1,1,0)
 	deathscreen.modulate=Color(0,0,0,0)
 	endscreen.modulate=Color(0,0,0,0)
+	pipesarc.modulate=Color(0,0,0,0)
 	if get_tree().current_scene.name=="bathfloor1":
 		_setopacity()
 
@@ -36,6 +38,7 @@ func _process(delta):
 		_return.disabled=false
 		_return.modulate=Color(1,1,1,1)
 		fade=true
+	_areacard()
 	
 
 func _on_timer_timeout():
@@ -53,9 +56,11 @@ func _tutorial():
 	Input.get_action_strength("down")-Input.get_action_strength("up")
 	)
 	if direction&&wasd:
+		await get_tree().create_timer(2).timeout
 		fade=true
 		_timer.start()
 	if (Input.get_action_strength("dash")):
+		await get_tree().create_timer(2).timeout
 		fade=true
 	if fade:
 		animated_sprite_2d.modulate.a=lerp(animated_sprite_2d.modulate.a,0.0,0.05)
@@ -64,6 +69,12 @@ func _tutorial():
 	else:
 		animated_sprite_2d.modulate.a=lerp(animated_sprite_2d.modulate.a,255.0,0.005)
 		label.modulate.a=lerp(label.modulate.a,255.0,0.005)
+		
+func _areacard():
+	if get_tree().current_scene.name=="pipes1":
+		pipesarc.modulate=lerp(pipesarc.modulate,Color(1,1,1,0.9),0.05)
+		await get_tree().create_timer(5).timeout
+		pipesarc.modulate.a=lerp(pipesarc.modulate.a,-1.0,0.05)
 
 func _on_retry_pressed():
 	get_tree().reload_current_scene()
@@ -74,7 +85,6 @@ func _on_exit_body_entered(body):
 		_return.disabled=false
 		_return.modulate=Color(1,1,1,1)
 		_mc._movelock=true
-		print("ee")
 
 func _on_return_pressed():
 	get_tree().change_scene_to_file("res://scenes/mainmenu.tscn")
--- Design Studio Game/scripts/roach.gd
+++ Design Studio Game/scripts/roach.gd
@@ -22,7 +22,6 @@ func _ready():
 	_updateanim(_spritedir)
 
 func _physics_process(delta):
-
 	var direction = Vector2(
 		Input.get_action_strength("right")-Input.get_action_strength("left"),
 		Input.get_action_strength("down")-Input.get_action_strength("up")
